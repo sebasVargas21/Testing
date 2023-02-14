a0be85e5b7ec070d7c7218b2e1b57542892a3304
Silvio Esterilla <silanger9@hotmail.com>
2023-02-13 19:43:50 -0500

    added eye and eye-slash when password is visible

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -6,31 +6,31 @@ function Login() {
   const [isVisible, setIsVisible] = React.useState(true)
   return (
     <div>
-        <form className={styles.login}>
-            <strong className={styles.loginLogo}>
-                Log In
+      <form className={styles.login}>
+        <strong className={styles.loginLogo}>
+          Log In
             </strong>
-            <input type="text" name="Email" placeholder='Email' required/>
-            <div className={styles.inputArea}><input type={isVisible ? "password" : "text"} name="Password" placeholder='Password'required/>
-            <i className={styles.iconEyePassword +' fa fa-eye'} onClick={(event)=> {
-              event.preventDefault()
-              setIsVisible(!isVisible)
-              }}></i></div>
-            
-            
-            <div className={styles.container}> 
-              <input className={styles.checkbox} type="checkbox" value="Keep me signed in" id='checkbox'/> <label htmlFor="checkbox">Keep me signed in</label>
-            </div>
-            <div className={styles.containerButton}>
-              <input type="submit" value="Log In" />
-            </div>
+        <input type="text" name="Email" placeholder='Email' required />
+        <div className={styles.inputArea}>
+          <input type={isVisible ? "password" : "text"} name="Password" placeholder='Password' required />
+          <i className={styles.iconEyePassword + ` ${isVisible ? 'fa fa-eye-slash' : 'fa fa-eye'}`} onClick={(event) => {
+            event.preventDefault()
+            setIsVisible(!isVisible)
+          }}></i>
+        </div>
+        <div className={styles.container}>
+          <input className={styles.checkbox} type="checkbox" value="Keep me signed in" id='checkbox' /> <label htmlFor="checkbox">Keep me signed in</label>
+        </div>
+        <div className={styles.containerButton}>
+          <input type="submit" value="Log In" />
+        </div>
 
-            <div className={styles.containerForgot}>
-              <a> Forgot Password?</a>
-            </div>
-            
+        <div className={styles.containerForgot}>
+          <a> Forgot Password?</a>
+        </div>
 
-        </form>
+
+      </form>
     </div>
   )
 }
